aefea72175765ba4dc0caf05f56ec81870fac118
Muhammad Abdullah <abdullah.kiet@gmail.com>
2020-06-21 14:36:40 +0500

    updated index and app.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 function App(props) {
   return (
     <div>
-      Hello World by {props.name}
+      Hello World by {props.name} age= {props.name-5}
       
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
- <div> <App name="Muhammad Abdullah"/> </div>,
+ <div> <App name="Muhammad Abdullah" age={25}/> </div>,
   document.getElementById('root')
 );
 
