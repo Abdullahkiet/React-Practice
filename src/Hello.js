bc02823e1f3cc4d612b027953ba177f3d82dd4bd
Muhammad Abdullah <abdullah.kiet@gmail.com>
2020-06-21 17:46:22 +0500

    Included hello.css

--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './Hello.css';
 
 function Hello({firstname}) {
-  return <p>
+  return <p className="myname">
       This text is from Hello.js made by: {firstname}
   </p>
 
