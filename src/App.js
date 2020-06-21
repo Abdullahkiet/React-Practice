4aeddb5421646f08004150c40bb3dd26d26e247d
Muhammad Abdullah <abdullah.kiet@gmail.com>
2020-06-21 15:07:01 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,36 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
+const hello=({name}) => "Hello world" + name;
+
+  
 
 function App(props) {
   return (
     <div>
       Hello World by {props.name} age= {props.age-5}
+      hello();
+      
+  
       
     </div>
+    
   );
+
+  //2nd way of writting above funtion
+
+  // function App(name,age) {
+  //   return (
+  //     <div>
+  //       Hello World by {name} age= {age-5}
+        
+  //     </div>
+  //   );
+
+  //we can write funtion using arrow
+
+  // const hello=({name}) => "Hello world" + name;
+  
 }
 
 export default App;
